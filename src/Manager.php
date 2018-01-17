b2c961c372923a1879980c866b41f728b4d460b6
lcherone <plinker@cherone.co.uk>
2018-01-17 10:39:26 +0000

    [since: v0.3.0]  17-Jan-2018 - .

--- src/Manager.php
+++ src/Manager.php
@@ -236,14 +236,14 @@ namespace Plinker\Iptables {
                 return trim(com_create_guid(), '{}');
             } else {
                 return sprintf(
-                    '%04x%04x-%04x-%04x-%04x-%04x%04x%04x', 
-                    mt_rand(0, 65535), 
-                    mt_rand(0, 65535), 
-                    mt_rand(0, 65535), 
-                    mt_rand(16384, 20479), 
-                    mt_rand(32768, 49151), 
-                    mt_rand(0, 65535), 
-                    mt_rand(0, 65535), 
+                    '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                    mt_rand(0, 65535),
+                    mt_rand(0, 65535),
+                    mt_rand(0, 65535),
+                    mt_rand(16384, 20479),
+                    mt_rand(32768, 49151),
+                    mt_rand(0, 65535),
+                    mt_rand(0, 65535),
                     mt_rand(0, 65535)
                 );
             }
