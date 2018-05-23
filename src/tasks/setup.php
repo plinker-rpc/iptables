b94ec95664bb495eabd16607d22cadb7e9be46d0
lcherone <plinker@cherone.co.uk>
2018-05-23 03:36:32 +0000

    [since: v1.1.4]  23-May-2018 - fixes

--- src/tasks/setup.php
+++ src/tasks/setup.php
@@ -15,3 +15,25 @@
  | Authors: Lawrence Cherone <lawrence@cherone.co.uk>                     |
  +------------------------------------------------------------------------+
  */
+// init fatfree instance
+$f3 = \Base::instance();
+
+// load config
+$f3->config('config.ini');
+
+$client = new \Plinker\Core\Client('http://127.0.0.1:88', [
+  'secret' => $f3->get('AUTH.secret'),
+    'database' => $f3->get('db')
+]);
+
+$client->iptables->setup([
+  'build_sleep' => 5,
+    'lxd' => [
+        'bridge' => 'lxcbr0',
+        'ip' => '10.171.90.0/8'
+    ],
+    'docker' => [
+        'bridge' => 'docker0',
+        'ip' => '172.17.0.0/16'
+    ]
+]);
